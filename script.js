f82229def5e737b5cf6cb07d67adee4f49c49cfc
Rune Samyn <rune.samyn@gmail.com>
2020-05-26 13:50:07 +0100

    show mqtt input on div's

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
-var client = mqtt.connect("ws://test.mosquitto.org:1883/ws")
+var client = mqtt.connect("ws://test.mosquitto.org:8080/ws")
+
 client.subscribe("CHANGE_DETECTED")
 
-client.on("message", function (topic, payload) {
-    alert([topic, payload].join(": "))
+client.on("message", function (topic, payload) {
+if(topic == "CHANGE_DETECTED")
+{
+    var data = JSON.parse(payload.toString());
+    if(data.port == 2){
+        if(data.value == 1){
+            document.getElementById("input1").style.backgroundColor = "green";
+        } else if(data.value == 0){
+            document.getElementById("input1").style.backgroundColor = "red";
+        }
+    } else if(data.port == 3){
+        if(data.value == 1){
+            document.getElementById("input2").style.backgroundColor = "green";
+        } else if(data.value == 0){
+            document.getElementById("input2").style.backgroundColor = "red";
+        }
+    }
+}
+//client.end()
 })
 
-client.publish("test_send", "hello world!")
+client.publish("SET_OUTPUT", "hello world!")
